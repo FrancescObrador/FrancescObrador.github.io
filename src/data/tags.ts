94739c6d1212706ba13314ff7939f71f97f24c68
Francesc Obrador <obrador.francesc@gmail.com>
2025-03-09 21:19:21 +0100

    Mejora sustancial. refactor completo

---
+++ src/data/tags.ts
@@ -0,0 +1,187 @@
+export const TAGS = {
+    unreal: {
+        name: 'Unreal Engine',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/Unreal Engine_light',
+    },
+    cpp: {
+        name: 'C++',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/c++',
+    },
+    csharp: {
+        name: 'C#',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/csharp',
+    },
+    javascript: {
+        name: 'JavaScript',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/javascript',
+    },
+    docker: {
+        name: 'Docker',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/docker',
+    },
+    angular: {
+        name: 'Angular',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/angular',
+    },
+    nestjs: {
+        name: 'NestJS',
+        class: 'bg-slate-700 text-white', 
+        icon: 'technologies/nestjs',
+    },
+    typescript: {
+        name: 'TypeScript',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/typescript',
+    },
+    unity_dark: {
+        name: 'Unity',
+        class: 'bg-slate-700 text-white', 
+        icon: 'technologies/Unity_dark',
+    },
+    css: {
+        name: 'CSS',
+        class: 'bg-slate-700 text-white', 
+        icon: 'technologies/css',
+    },
+    html: {
+        name: 'HTML',
+        class: 'bg-slate-700 text-white', 
+        icon: 'technologies/html5',
+    },
+    git: {
+        name: 'Git',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/git',
+    },
+    swift: {
+        name: 'Swift',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/swift',
+    },
+    postgres: {
+        name: 'PostgreSQL',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/postgresql',
+    },
+    tailwind: {
+        name: 'Tailwind',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/tailwindcss',
+    },
+    typeorm: {
+        name: 'TypeORM',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/typeorm',
+    },
+    react: {
+        name: 'React',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/react',
+    },
+    firebase: {
+        name: 'Firebase',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/firebase',
+    },
+    redux: {
+        name: 'Redux',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/redux',
+    },
+    bootstrap: {
+        name: 'Bootstrap',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/bootstrap',
+    },
+    sqlserver: {
+        name: 'SQL Server',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/sql-server',
+    },
+    vue: {
+        name: 'Vue',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/vue',
+    },
+    spring: {
+        name: 'Spring',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/spring',
+    },
+    java: {
+        name: 'Java',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/java',
+    },
+    mysql: {
+        name: 'MySQL',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/mysql',
+    },
+    azure: {
+        name: 'Azure',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/azure',
+    },
+    reactrouter: {
+        name: 'React Router',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/reactrouter',
+    },
+    jest: {
+        name: 'Jest',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/jest',
+    },
+    nodejs: {
+        name: 'Node.js',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/nodejs',
+    },
+    postman: {
+        name: 'Postman',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/postman',
+    },
+    mongodb: {
+        name: 'MongoDB',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/mongodb',
+    },
+    vitejs: {
+        name: 'Vite',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/vitejs',
+    },
+    dotnet: {
+        name: '.NET',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/dotnet',
+    },
+    aspnet: {
+        name: 'ASP.NET',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/aspnet',
+    },
+    daisyui: {
+        name: 'DaisyUI',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/daisyui',
+    },
+    aws_dark: {
+        name: 'AWS',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/aws_dark',
+    },
+    aws_light: {
+        name: 'AWS',
+        class: 'bg-slate-700 text-white',
+        icon: 'technologies/aws_light',
+    }
+}
