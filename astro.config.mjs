cd00d45eefcbcd26b1f911e6962b8dd217ce9fbb
Francesc Obrador <obrador.francesc@gmail.com>
2025-03-10 10:56:12 +0100

    intento build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  outDir: "./dist",
   devOptions: {
     port: process.env.PORT | 3000,
     host: '0.0.0.0',
